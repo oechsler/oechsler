4b3fb92cb0112b78261b9db2d9e57ff0748800b2
Samuel Oechsler <samuel@oechsler.it>
2019-08-16 18:10:42 +0200

    Fix typo

--- src/components/footer/FooterAzure.tsx
+++ src/components/footer/FooterAzure.tsx
@@ -6,7 +6,7 @@ import azure from '../../images/azure-logo.png';
 const FooterAzure = () => {
     return (
         <div className='footer-azure'>
-            <p>Delivered from the cloud with</p>
+            <p>Delivered from the cloud by</p>
             <Image src={azure} href='https://azure.com' as='a' />
         </div>
     );
