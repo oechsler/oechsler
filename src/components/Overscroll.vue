779aa1b27b8e7a7b4bae4953b444bf862496ca24
Samuel Oechsler <samuel@oechsler.it>
2020-04-10 13:47:36 +0200

    Fix bug on over-scroll with iPads

--- src/components/Overscroll.vue
+++ src/components/Overscroll.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overscroll">
+  <div :class="['overscroll', { invisible: scrollTop > 0 }]">
     <p class="overscroll-message" v-html="message"></p>
   </div>
 </template>
@@ -10,6 +10,22 @@ import { Vue, Component, Prop } from "vue-property-decorator";
 @Component
 export default class Overscroll extends Vue {
   @Prop({ required: true }) readonly message!: string;
+
+  private scrollTop = 0;
+
+  public mounted() {
+    window.addEventListener("scroll", this.onScroll, { passive: true });
+  }
+
+  public beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
+  }
+
+  private onScroll() {
+    const element = document.documentElement;
+    this.scrollTop =
+      (window.pageYOffset || element.scrollTop) - (element.clientTop || 0);
+  }
 }
 </script>
 
@@ -19,10 +35,14 @@ export default class Overscroll extends Vue {
   @apply top-0;
   @apply left-1/2;
   @apply -ml-150;
-  @apply p-12;
+  @apply pt-12;
   @apply w-300;
 
   @apply -z-10;
+
+  &.invisible {
+    @apply hidden;
+  }
 
   .overscroll-message {
     @apply font-bold;
